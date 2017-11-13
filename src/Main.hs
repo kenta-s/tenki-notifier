ca7b98bf2bcdbbe84c9f4a0bff923a2097ef1ab9
kenta-s <knt01222@gmail.com>
2017-11-13 09:26:14 +0900

    Make it work

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import           Data.Aeson            (Value)
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.Yaml             as Yaml
+import qualified Network.HTTP.Simple   as HS
+import           System.Environment    (getEnv)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  appId <- getEnv "YOLP_APP_ID"
+  initReq <- HS.parseRequest "https://map.yahooapis.jp/weather/V1/place?coordinates=139.732293,35.663613&output=json"
+  let userAgent = S8.pack $ "Yahoo AppID: " ++ appId
+      req = HS.setRequestHeaders [("User-Agent", userAgent)] initReq
+  response <- HS.httpJSON req
+  putStrLn $ "The status code was: " ++ show (HS.getResponseStatusCode response)
+  print $ HS.getResponseHeader "Content-Type" response
+  S8.putStrLn $ Yaml.encode (HS.getResponseBody response :: Value)
