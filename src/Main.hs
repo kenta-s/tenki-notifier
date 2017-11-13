4159c21f418202b4f4fbcd7553aef026b4cd240d
kenta-s <knt01222@gmail.com>
2017-11-14 08:18:37 +0900

    Change from YOLP to OpenWeatherMap

--- src/Main.hs
+++ src/Main.hs
@@ -9,13 +9,8 @@ import           System.Environment    (getEnv)
 
 main :: IO ()
 main = do
-  appId <- getEnv "YOLP_APP_ID"
-  let lon = "35.658034"
-      lat = "139.701636"
-  initReq <- HS.parseRequest $ "https://map.yahooapis.jp/weather/V1/place?output=json&coordinates=" ++ lat ++ "," ++ lon
-  let userAgent = S8.pack $ "Yahoo AppID: " ++ appId
-      req = HS.setRequestHeaders [("User-Agent", userAgent)] initReq
+  appId <- getEnv "OPEN_WEATHER_MAP_API_KEY"
+  let tokyoId = "1850147"
+  req <- HS.parseRequest $ "http://api.openweathermap.org/data/2.5/forecast?id=" ++ tokyoId ++ "&APPID=" ++ appId
   response <- HS.httpJSON req
-  putStrLn $ "The status code was: " ++ show (HS.getResponseStatusCode response)
-  print $ HS.getResponseHeader "Content-Type" response
   S8.putStrLn $ Yaml.encode (HS.getResponseBody response :: Value)
