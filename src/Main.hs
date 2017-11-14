7ace52d697e0b0af830f95acf66e5ec185f357ac
kenta-s <knt01222@gmail.com>
2017-11-14 09:06:57 +0900

    parse JSON

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
-import           Data.Aeson            (Value)
+import           Data.Aeson
 import qualified Data.ByteString.Char8 as S8
-import qualified Data.Yaml             as Yaml
 import qualified Network.HTTP.Simple   as HS
 import           System.Environment    (getEnv)
+import           Data.Text
+import           GHC.Generics
+
+data WeatherListWrapper = WeatherListWrapper {
+  list :: WeatherList
+} deriving (Generic, Show)
+instance FromJSON WeatherListWrapper
+
+data WeatherList = WeatherList {
+  weather :: Weather,
+  dt_txt :: Text
+} deriving (Generic, Show)
+instance FromJSON WeatherList
+
+data Weather = Weather {
+  mainInfo :: Text,
+  description :: Text
+} deriving (Generic, Show)
+instance FromJSON Weather where
+    parseJSON (Object v) = Weather <$> (v .: "main") <*> (v .: "description")
 
 main :: IO ()
 main = do
@@ -13,4 +34,4 @@ main = do
   let tokyoId = "1850147"
   req <- HS.parseRequest $ "http://api.openweathermap.org/data/2.5/forecast?id=" ++ tokyoId ++ "&APPID=" ++ appId
   response <- HS.httpJSON req
-  S8.putStrLn $ Yaml.encode (HS.getResponseBody response :: Value)
+  print $ list (HS.getResponseBody response)
